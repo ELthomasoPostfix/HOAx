ff9f2a41f327361babcc91cad81631156923cb10
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2025-01-06 19:46:38 +0100

    Add bdd utils.
    Add utils for:
      * extracting a conjunction of all variables
        used in a prop. formula
      * getting an array the var indexes of all
        variables used in a prop. formula

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,3 +13,44 @@ bool operator<(const HOAxState &s1, const HOAxState &s2) {
     // Resort to comparing bdd IDs to break equality ties.
     return (s1.first < s2.first) || (s1.second.id() < s2.second.id());
 }
+
+bdd bdd_variables(const bdd &r) {
+    return bdd_variables_(r, true);
+}
+
+bdd bdd_variablescomp(const bdd &r) {
+    return bdd_variables_(r, false);
+}
+
+bdd bdd_variables_(const bdd &r, const bool var_should_appear) {
+    int *vprof = bdd_varprofile(r);
+
+    /* Use a boolean rule: T & a = a */
+    bdd conjunction = bddtrue;
+    for (size_t i = 0; i < bdd_varnum(); i++) {
+        bool var_appears = (*(vprof + i) > 0);
+        if (var_appears == var_should_appear)
+            conjunction &= bdd_ithvar(i);
+    }
+
+    if (vprof) free(vprof);    // spot does not free this mem.
+    return conjunction;
+}
+
+void bdd_var_indexes(const bdd &r, int **indexes, int *size) {
+    int *vprof = bdd_varprofile(r);
+
+    /* First pass: find out how many variables appear. */
+    size_t count = 0;
+    for (int i = 0; i < bdd_varnum(); i++)
+        count += (*(vprof + i) > 0);
+
+    /* Second pass: fill the array. */
+    *size = count;
+    *indexes = new int[count];
+    for (int i = 0; i < bdd_varnum(); i++)
+        if (*(vprof + i) > 0)
+            **indexes = i;
+
+    if (vprof) free(vprof);    // spot does not free this mem.
+}
--- src/utils.h
+++ src/utils.h
@@ -16,4 +16,43 @@ std::ostream &operator<<(std::ostream &os, const HOAxState &state);
 /** Return true iff. s1 < s2. */
 bool operator<(const HOAxState &s1, const HOAxState &s2);
 
+/** Find the conjunction of all variables that appear in the bdd.
+
+    For example, given 5 variables: a, b, c, d, e
+        a | (e & c) | !a    ==>    a & c & e
+
+    @param[in] r The bdd to examine
+    @return The conjunction bdd
+ */
+bdd bdd_variables(const bdd &r);
+
+/** Find the conjunction of all variables that do not appear in the bdd.
+
+    For example, given 5 variables: a, b, c, d, e
+        a | (e & c) | !a    ==>    b & d
+
+    @param[in] r The bdd to examine
+    @return The conjunction bdd
+ */
+bdd bdd_variablescomp(const bdd &r);
+
+/** Find the conjunction of all variables that (do not) appear in the bdd.
+
+    @param[in] r The bdd to examine
+    @param[in] var_should_appear If true, then add variables that do appear.
+                                 If false, then add variables that do not.
+    @return The conjunction bdd
+*/
+bdd bdd_variables_(const bdd &r, const bool var_should_appear);
+
+/** Find the variable index of each of the variables that appear in the bdd.
+
+    The memory allocated for the indexes array must be freed by the caller.
+
+    @param[in] r The bdd to examine
+    @param[out] indexes The pointer that the created array is assigned to
+    @param[out] size The pointer that the array size is assigned to
+*/
+void bdd_var_indexes(const bdd &r, int **indexes, int *size);
+
 #endif
