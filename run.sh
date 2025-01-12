8b1e20eb29b00d1bc709886c2ae45deddc35db64
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2025-01-12 16:14:50 +0100

    Refactor run.sh + add basic test benchmarks.
    * Refactor run.sh to use the time
      command with each invocation of
      './builddir/hoax' to get an
      indication of program runtime
    * Also echo the current system time
      before and after all invocations
      of the entrypoint in run.sh, as
      a general indication of total
      runtime
    * Add new test benchmarks to
      'input/hoa_benchmarks'.
      They are select inputs from the
      keiren and tlsf_based subdirs of
      the benchmarks. They are proper
      inputs for resp. 'max even' with
      2 Accepting sets (ACs), 'max even'
      with 3 ACs, 'min odd' with 2 ACs
      and 'min odd' with 3 ACs.
    ==> Currently my implementation has
     issues with solving an even number
     of ACs. This likely has to do with
     the order of the W0, W1 sets output
     by zielonka (i.e. the std::swap calls
     in zielonka).

--- run.sh
+++ run.sh
@@ -11,14 +11,25 @@ then
     fi
 fi
 
+echo "[hoax] start at $(date +"%T")"
 # The "--brief" arg is used to run the smallest benchmark subdir, for testing.
 if [[ $* == *--brief* ]]
 then
-    ./builddir/hoax -v -b input/parity/parity/mucalc_mc/*.ehoa
-    ./builddir/hoax -v -b input/hoa_benchmarks/model_paper-2.property4-*.ehoa
+    # Modified versions of a mucalc_mc input, to see if the tool
+    # crashes and burns if it sees anything other than "marity max even".
+    time (./builddir/hoax -v -b input/hoa_benchmarks/model_paper-2.property4-*.ehoa)
+    # A selection of keiren and tlsf_based inputs, which are actual instances of:
+    #   a) "parity min odd"  with #ACCs = 2 or 3
+    #   b) "parity max even" with #ACCs = 2 or 3
+    time (./builddir/hoax -v -b input/hoa_benchmarks/diff-ACCs-*.ehoa)
+    # The entire mucalc_mc subdir of the actual benchmarks.
+    time (./builddir/hoax -v -b input/parity/parity/mucalc_mc/*.ehoa)
 else
-    ./builddir/hoax -v input/parity/parity/keiren/*.ehoa
-    ./builddir/hoax -v input/parity/parity/mucalc_mc/*.ehoa
-    ./builddir/hoax -v input/parity/parity/pgsolver_based/*.ehoa
-    ./builddir/hoax -v input/parity/parity/tlsf_based/*.ehoa
+    # Run the entirety of the benchmarks. This requires the benchmarks to
+    # be locally available at the relative path "./input/parity/parity/"
+    time (./builddir/hoax -v input/parity/parity/keiren/*.ehoa)
+    time (./builddir/hoax -v input/parity/parity/mucalc_mc/*.ehoa)
+    time (./builddir/hoax -v input/parity/parity/pgsolver_based/*.ehoa)
+    time (./builddir/hoax -v input/parity/parity/tlsf_based/*.ehoa)
 fi
+echo "[hoax] finished at $(date +"%T")"
